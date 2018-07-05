f95bd527790991d7e121b82ab0a92065183dc5bf
EC2 Default User <ec2-user@ip-172-31-32-232.us-east-2.compute.internal>
2018-07-05 06:35:35 +0000

    byenavi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,20 @@
-<html>
-    <h1>Hello, We Are Medium Rare (@.@)b</h1>
-</html>
+@extends('layouts.app')
+
+@section('cover')
+@endsection
+
+@section('content')
+    <div class="cover">
+        <div class="cover-inner">
+            <div class="cover-contents">
+
+            
+                <a href="{{ route('signup.get') }}" class="bt"><div id="a">Sign Up</div></a>
+                
+                <a href="{{ route('login') }}" class="bt"><div id="a">Log In</div></a>
+                
+            </div>
+        </div>
+    </div>
+  
+@endsection
