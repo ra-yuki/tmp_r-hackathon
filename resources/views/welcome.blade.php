cc38e4334a44768b27ca09524b366977ae686583
miyuokada <miyu.a.okada@rakuten.com>
2018-07-09 05:53:36 +0000

    welcompage design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,18 +8,31 @@
         <div class="cover-inner">
             <div class="cover-contents">
 
-                  
-                <div class="img">
-                <img src={{ secure_asset('images/pp.gif') }}>
-                </div>
+ <h1 class="ml1">
+    <span class="text-wrapper">
+    <span class="line line1"></span>
+    <span class="letters">CALENDAR</span>
+    <span class="line line2"></span>
+    </span>
+</h1>
+               <p class="p">Everyone's schedules on one calender</p>
                 
+
+                <a href="{{ route('signup.get') }}" id="a">Sign up</a>
                 
-                <a href="{{ route('signup.get') }}" class="bt"><div id="a">Sign Up</div></a>
+                <a href="{{ route('login') }}" id="b">Log In</a>
                 
-                <a href="{{ route('login') }}" class="bt"><div id="a">Log In</div></a>
-                
-            </div>
-        </div>
-    </div>
+
   
+               
+            </div>
+            
+        </div>
+        <a href="/" id="what">What is CALENDER?</a>
+        
+    </div>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
+<script src="{{ secure_asset('js/test.js') }}"></script>     
 @endsection
