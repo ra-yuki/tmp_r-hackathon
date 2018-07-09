60b756bb023c67d1bfc787b38eba93e285ab3b26
meh <meh>
2018-07-09 23:45:52 +0000

    changed events table

---
+++ database/migrations/2018_07_10_083234_update180710_events_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Update180710EventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('events', function (Blueprint $table) {
+            // news
+            $table->dateTime('dateTimeFromSelf')->nullable();
+            $table->dateTime('dateTimeToSelf')->nullable();
+            // changes
+            $table->date('dateFrom')->nullable()->change();
+            $table->date('dateTo')->nullable()->change();
+            $table->time('timeFrom')->nullable()->change();
+            $table->time('timeTo')->nullable()->change();
+            $table->string("location")->nullable()->change();
+            $table->text("description")->nullable()->change();
+            $table->integer('linkId')->nullable()->change();
+            $table->dropColumn("groupId");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('events', function (Blueprint $table) {
+            // news
+            $table->dropColumn('dateTimeFromSelf');
+            $table->dropColumn('dateTimeToSelf');
+            // changes
+            $table->date('dateFrom')->nullable(false)->change();
+            $table->date('dateTo')->nullable(false)->change();
+            $table->time('timeFrom')->nullable(false)->change();
+            $table->time('timeTo')->nullable(false)->change();
+            $table->string("location")->nullable(false)->change();
+            $table->text("description")->nullable(false)->change();
+            $table->integer('linkId')->nullable(false)->change();
+            $table->integer("groupId");
+        });
+    }
+}
